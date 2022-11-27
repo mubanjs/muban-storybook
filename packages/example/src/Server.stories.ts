ef4354a05992864799a8c199f91d3809bda26e3f
Arjan van Wijk <thanarie@gmail.com>
2022-11-27 16:58:14 +0100

    Add support for storybook actions
    When `argTypes` are defined as `action` (which still needs to be done
    manually for each component), Storybook will pass action handlers as
    `args`, that output the passed data from these function callbacks into
    the Actions Panel.
    
    In our `render` function, we filter out those specific `args` based on
    the information in the `argTypes`, and pass these to our rendered
    component via `setProps`.
    
    Now, whenever a callback function is called inside the component,
    these action handlers are being triggered.
    
    Fixes #9

--- packages/example/src/Server.stories.ts
+++ packages/example/src/Server.stories.ts
@@ -1,20 +1,21 @@
-import type { TemplateStoryProps, Story, Meta } from '@muban/storybook';
-import { StoryComponent, storyTemplate } from './Component';
+import type { TemplateStoryProps, Story, Meta } from "@muban/storybook";
+import { StoryComponent, storyTemplate } from "./Component";
+import { argTypes } from "./StoryComponent.argTypes";
 
 export default {
-  title: 'Server',
+  title: "Server",
   component: StoryComponent,
+  argTypes,
 } as Meta;
 
 // Render on the server!
 export const Simple: Story<TemplateStoryProps<typeof storyTemplate>> = {
   parameters: {
     server: {
-      id: 'useToggle',
+      id: "useToggle",
     },
   },
   args: {
     initialValue: true,
-  }
-}
-
+  },
+};
--- packages/example/src/Test.stories.ts
+++ packages/example/src/Test.stories.ts
@@ -1,31 +1,38 @@
-import type { TemplateStoryProps, StoryObj, Story, Meta } from '@muban/storybook';
-import { StoryComponent, storyTemplate } from './Component';
+import type {
+  TemplateStoryProps,
+  StoryObj,
+  Story,
+  Meta,
+} from "@muban/storybook";
+import { StoryComponent, storyTemplate } from "./Component";
+import { argTypes } from "./StoryComponent.argTypes";
 
 export default {
-  title: 'CSF v3',
+  title: "CSF v3",
+  argTypes,
 } as Meta;
 
 // Storybook 7.x Story Object - CSFv3
 export const ClientObj: StoryObj<TemplateStoryProps<typeof storyTemplate>> = {
   render: () => ({
     component: StoryComponent,
-    template: storyTemplate
+    template: storyTemplate,
   }),
   args: {
     initialValue: false,
-  }
-}
+  },
+};
 
 // Imported from types-7-0 - render on the client
 export const ClientStory: Story<TemplateStoryProps<typeof storyTemplate>> = {
   render: () => ({
     component: StoryComponent,
-    template: storyTemplate
+    template: storyTemplate,
   }),
   args: {
     initialValue: false,
-  }
-}
+  },
+};
 
 // Custom data
 export const CustomData: Story<TemplateStoryProps<typeof storyTemplate>> = {
@@ -34,9 +41,9 @@ export const CustomData: Story<TemplateStoryProps<typeof storyTemplate>> = {
     template: storyTemplate,
     // change/append data inside here, which is again passed to the template function
     // this could also be done directly inside the template function
-    data: {...args, initialValue: true},
- }),
+    data: { ...args, initialValue: true },
+  }),
   args: {
     initialValue: false,
-  }
-}
+  },
+};
