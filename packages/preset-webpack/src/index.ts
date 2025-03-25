0cd7fcb8fcb4fc435aad30d79d92bb415b27ceb4
Braian <braian.rosas@monks.com>
2025-03-25 07:53:35 -0300

    Add a rule check to add additional loaders to preset rules

--- packages/preset-webpack/src/index.ts
+++ packages/preset-webpack/src/index.ts
@@ -3,16 +3,23 @@ import type { StorybookConfig } from './types.js';
 export * from './types.js';
 
 export const webpack: StorybookConfig['webpack'] = (config) => {
-  config.module?.rules?.[2].use.push({
-    loader: 'ts-loader',
-    options: {
-      // makes sure to load only the files required by webpack and nothing more
-      onlyCompileBundledFiles: true,
-      // type checking is handled by `fork-ts-checker-webpack-plugin`
-      transpileOnly: true,
-      happyPackMode: true,
-    },
-  });
+  if (config.module?.rules) {
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule.test?.toString().includes('tsx?') && rule.use && Array.isArray(rule.use)) {
+        rule.use.push({
+          loader: 'ts-loader',
+          options: {
+            // makes sure to load only the files required by webpack and nothing more
+            onlyCompileBundledFiles: true,
+            // type checking is handled by `fork-ts-checker-webpack-plugin`
+            transpileOnly: true,
+            happyPackMode: true,
+          },
+        });
+      }
+      return rule;
+    });
+  }
 
   return config;
 };
